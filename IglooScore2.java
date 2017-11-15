1dbac9464ae652576844118bc3b27286b81f799e
Xitlally Salmon <31227186+xsalmon@users.noreply.github.com>
2017-11-14 17:55:07 -0700

    final submission

--- IglooScore2.java
+++ IglooScore2.java
@@ -58,7 +58,7 @@ public int overallScore()
 public static void main(String args[])
 {
    int s1, s2, s3;
-   if (args==null || args.length != 3) {
+   if (args.length != 3) {
       System.err.println("Error: must supply three arguments!");
       return;
    }
